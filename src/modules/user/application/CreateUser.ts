79d29961c77b1ffa29e00619c4c82f3db7d95f42
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-11-29 14:33:37 -0300

    feat: Caso de uso da aplicação

--- src/modules/user/application/CreateUser.ts
+++ src/modules/user/application/CreateUser.ts
@@ -15,5 +15,21 @@ interface ICreateUserDTO {
 @injectable()
 export class CreateUser {
 
+    constructor(@inject(Tokens.UserRepository) private userRepository: IUserRepository,
+    @inject(Tokens.HashProvider) private hashProvider: IHashProvider){};
 
+    async execute({name, email, cpf, password}: ICreateUserDTO): Promise<User> {
+
+        const userExists = await this.userRepository.findByEmail(email);
+
+        if(userExists) {
+
+            throw new Error("User already exists!");
+        };
+
+        const passwordHash = await this.hashProvider.hash(password);
+        const user = await this.userRepository.create({ name, email, cpf, passwordHash})
+        
+        return user;
+    };
 };
